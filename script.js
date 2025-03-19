0255346688d86d904af14d438945cc499b5ab120
unknown <saifurrahimwp@gmail.com>
2025-03-19 23:24:25 +0700

    Create side bar and its flip animation

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function toggleMobileMenu(){
+function toggleMobileTopMenu(){
     let navMenu = document.querySelector('nav div:nth-child(3)');
 
+    if(navMenu.classList.contains('hidden'))
+    {
+        document.querySelector('aside').classList.add('hidden');
+    }
+
     navMenu.classList.toggle('hidden');
+}
+
+function toggleMobileSideMenu(){
+    let navMenu = document.querySelector('aside');
+
+    if(navMenu.classList.contains('hidden'))
+    {
+        document.querySelector('nav div:nth-child(3)').classList.add('hidden');
+    }
+
+    navMenu.classList.toggle('hidden');
+
+    let sideToggler = document.querySelector('#menuToggler .fa-chevron-right');
+
+    sideToggler.style.transform = sideToggler.style.transform == 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
 }
