446d0e621030e9ba7d27d444085895f6f8ccbbe1
unknown <saifurrahimwp@gmail.com>
2025-03-25 11:33:43 +0700

    Implement sidebar and its toggler

--- js/topbar.js
+++ js/topbar.js
@@ -2,9 +2,27 @@ document.getElementById('topMenuToggler').addEventListener('change', (event) => 
 
     let topBarMenu = document.querySelector('header nav .menu');
     if(event.target.checked){
-        topBarMenu.style.display = 'flex';
+        let sideMenuToggler = document.getElementById('sideMenuToggler');
+        sideMenuToggler.checked = false;
+        sideMenuToggler.dispatchEvent(new Event('change')); 
+
+        topBarMenu.classList.add('appear');
     }
     else{
-        topBarMenu.style.display = 'none';
+        topBarMenu.classList.remove('appear');
+    }
+});
+
+document.getElementById('sideMenuToggler').addEventListener('change', (event) => {
+
+    let sideBarMenu = document.querySelector('aside');
+    if(event.target.checked){
+        let topMenuToggler = document.getElementById('topMenuToggler');
+        topMenuToggler.checked = false;
+        topMenuToggler.dispatchEvent(new Event('change'));
+        sideBarMenu.classList.add('appear');
+    }
+    else{
+        sideBarMenu.classList.remove('appear');
     }
 })
